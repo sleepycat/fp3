3288ddb6819e0761ec5d1155ca7c681cb2df1832
Mike Williamson <mike@korora.ca>
2025-03-27 16:53:27 -0400

    Clean up a few typescript errors.

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,16 @@
 import { lingui } from "@lingui/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
+/// <reference types="panda" />
 import pandacss from "@pandacss/dev/postcss";
 import { defineConfig } from "vite";
 import macrosPlugin from "vite-plugin-babel-macros";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-declare module "@remix-run/node" {
-	// or cloudflare, deno, etc.
-	interface Future {
-		v3_singleFetch: true;
-	}
-}
 
 export default defineConfig({
 	css: { postcss: { plugins: [pandacss, autoprefixer] } },
 	server: {
 		port: 3000,
 	},
-	plugins: [
-		reactRouter({
-			future: {
-				v3_fetcherPersist: true,
-				v3_relativeSplatPath: true,
-				v3_throwAbortReason: true,
-				v3_singleFetch: true,
-				v3_lazyRouteDiscovery: true,
-			},
-		}),
-		macrosPlugin(),
-		lingui(),
-		tsconfigPaths(),
-	],
+	plugins: [reactRouter(), macrosPlugin(), lingui(), tsconfigPaths()],
 });
